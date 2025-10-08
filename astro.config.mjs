6429f1a8a47eb06bbdd790b41a49a2b75e0605f2
Garrett Pierce <31076033+gfpierce@users.noreply.github.com>
2025-10-08 13:47:04 -0400

    Fixed some silly removals fueled by React hate

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,21 @@ import tailwind from "@astrojs/tailwind";
 
 import sanity from "@sanity/astro";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
     responsiveStyles: false,
   },
-  integrations: [
-    alpinejs(),
-    tailwind(),
-    sanity({
-      projectId: "bzkb1ks8",
-      dataset: "production",
-      useCdn: false,
-      apiVersion: "2025-08-18",
-      studioBasePath: "/studio",
-      stega: {
-        studioUrl: "/studio",
-      },
-    }),
-  ],
+  integrations: [alpinejs(), tailwind(), sanity({
+    projectId: "bzkb1ks8",
+    dataset: "production",
+    useCdn: false,
+    apiVersion: "2025-08-18",
+    studioBasePath: "/studio",
+    stega: {
+      studioUrl: "/studio",
+    },
+  }), react()],
 });
